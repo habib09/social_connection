a73ffe0be2e09f7967bd7cd9de861d3fa4088b41
habib09 <habib09022@gmail.com>
2020-10-03 00:34:40 +0600

    login-logout functionality added

--- blog/views.py
+++ blog/views.py
@@ -1,25 +1,13 @@
 from django.shortcuts import render
+from .models import Post
 
-posts = [
-    {
-        'author': 'bossri',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'August 22, 2020'
-    },
-    {
-        'author': 'habib',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'August 23, 2020'
-    }
-]
+
 def home(request):
-
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
+
 
 def about(request):
     return render(request, 'blog/about.html', {'title': 'About'})
